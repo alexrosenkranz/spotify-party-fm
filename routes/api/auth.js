e2956c783e3c5e3bbab402d8ecc2cf6aeca82603
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-07-16 18:45:53 -0400

    setup for branching

---
+++ routes/api/auth.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const passport = require('passport');
+const usersController = require('../../controllers/usersController');
+
+router
+  .route('/spotify')
+  .get(passport.authenticate('spotify', { scope: ['user-read-email', 'user-read-private']}), function (req, res) {
+    // The request will be redirected to spotify for authentication, so this
+    // function will not be called.
+  });
+
+router
+  .route('/spotify/callback')
+  .get(passport.authenticate('spotify', { failureRedirect: '/login'}), function (req, res) {
+    console.log(req.query);
+    // Successful authentication, redirect home.
+    res.json(req.user);
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const session = require('express-session');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const SpotifyStrategy = require('passport-spotify').Strategy;
+require('dotenv').config();
 
 // Import routes
 const routes = require('./routes');
@@ -27,16 +28,40 @@ app.use(express.static('client/build'));
 // Add routes, both API and route to client/build
 app.use(routes);
 
+// Passport session setup.   To support persistent login sessions, Passport
+// needs to be able to   serialize users into and deserialize users out of the
+// session. Typically,   this will be as simple as storing the user ID when
+// serializing, and finding   the user by ID when deserializing. However, since
+// this example does not   have a database of user records, the complete spotify
+// profile is serialized   and deserialized.
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+
 // Set up passport to authenticate
 const User = require('./models/user');
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.use(new SpotifyStrategy({
+  clientID: process.env.SPOTIFYCLIENT,
+  clientSecret: process.env.SPOTIFYSECRET,
+  callbackURL: "http://localhost:3001/api/auth/spotify/callback"
+},
+  (accessToken, refreshToken, expires_in, profile, done) => {
+    User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/spotify-fm');
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
