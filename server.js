9bf5f0c209b7b40cbc67ee551f7697ec95638df4
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-07-23 21:34:50 -0400

    working spotify authentication and refresh tokens

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const morgan = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const passport = require('passport');
-const SpotifyStrategy = require('passport-spotify').Strategy;
 require('dotenv').config();
 
 // Import routes
@@ -16,46 +16,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
 app.use(morgan('dev')); // for logging
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 } // Add routes, both API and route to client/build
 app.use(routes);
-
-// Passport session setup.   To support persistent login sessions, Passport
-// needs to be able to   serialize users into and deserialize users out of the
-// session. Typically,   this will be as simple as storing the user ID when
-// serializing, and finding   the user by ID when deserializing. However, since
-// this example does not   have a database of user records, the complete spotify
-// profile is serialized   and deserialized.
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
-// Set up passport to authenticate
-const User = require('./models/user');
-
-passport.use(new SpotifyStrategy({
-  clientID: process.env.SPOTIFYCLIENT,
-  clientSecret: process.env.SPOTIFYSECRET,
-  callbackURL: 'http://localhost:3001/api/auth/spotify/callback'
-}, (accessToken, refreshToken, expiresIn, profile, done) => {
-  console.log(profile);
-  User.findOrCreate({
-    spotifyId: profile.id
-  }, (err, user) => done(err, user));
-}));
 
 mongoose.Promise = Promise;
 
