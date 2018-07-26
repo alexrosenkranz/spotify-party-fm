3c8b610c7e9589c10938b34d036c17528ffd0007
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-07-25 23:00:06 -0400

    threw in bootstrap for quick ui organization

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
   // get params out of url
   getHashParams = () => {
     const hashParams = {};
-    console.log(window.location)
     let e,
       r = /([^&;=]+)=?([^&;]*)/g,
       q = window
@@ -24,6 +23,11 @@ class App extends Component {
     while (e = r.exec(q)) {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
+    // remove hash stuff
+    window
+      .history
+      .pushState("", document.title, window.location.pathname + window.location.search);
+
     return hashParams;
   }
 
@@ -65,15 +69,62 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a href="http://localhost:3001/api/auth/login">Log Into Spotify!</a><br/>
-
-        <button onClick={this.getRefreshToken}>Refresh Token!</button><br/>
-        <button onClick={this.getSpotifyProfile}>Get User Info</button><br/>
-        <button onClick={this.getSpotifyPlaylists}>Get Playlist Info</button><br/> {this.state.userInfo
-          ? JSON.stringify(this.state.userInfo, null, 2)
-          : "Login!!!"}<br/> {this.state.playlistData
-          ? JSON.stringify(this.state.playlistData, null, 2)
-          : "Get Playlists!!!"}
+        <div className="jumbotron jumbotron-fluid">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-12 text-align-center">
+              {this.state.userInfo
+                ? (
+                  <h2>Welcome {this.state.userInfo.display_name}!</h2>
+                )
+                : ""}
+              {!this.state.access_token
+                ? (
+                  <a
+                    className="btn btn-lg btn-success"
+                    href="http://localhost:3001/api/auth/login">Log Into Spotify!</a>
+                )
+                : (
+                  <div>
+                    <button className="btn btn-lg btn-danger" onClick={this.getRefreshToken}>
+                      Get Refresh Token
+                    </button>
+                    <button className="btn btn-lg btn-success" onClick={this.getSpotifyProfile}>
+                      Get User Info
+                    </button>
+                    <button className="btn btn-lg btn-info" onClick={this.getSpotifyPlaylists}>
+                      Get Playlists
+                    </button>
+                  </div>
+                )}
+            </div>
+          </div>
+        </div>
+        <div className="container">
+        <div className="row">
+          {this.state.playlistData
+            ? (this.state.playlistData.items.map(playlist => {
+              return (
+                <div className="col-3" key={playlist.id}>
+                  <div className="card">
+                    <img
+                      className="card-img-top"
+                      src={playlist.images[0].url}
+                      alt="playlist image"/>
+                    <div className="card-body">
+                      <h5 className="card-title">{playlist.name}</h5>
+                      <p className="card-text">{playlist.tracks.total
+                          ? playlist.tracks.total
+                          : 0}
+                        Tracks</p>
+                      <button className="btn btn-primary">Load Playlist</button>
+                    </div>
+                  </div>
+                </div>
+              )
+            }))
+            : ""}
+        </div>
+        </div>
       </div>
     );
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
 export default {
-  /*
-    loginCreds = {username: "alex", "password": 12345Password!}
-  */
   refreshToken : function (refreshToken) {
     return axios.get('/api/auth/refresh', {
       params: {
@@ -19,7 +16,7 @@ export default {
     })
   },
   getSpotifyPlaylists : function (accessToken) {
-    return axios.get(`https://api.spotify.com/v1/me/playlists`,{
+    return axios.get(`https://api.spotify.com/v1/me/playlists`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
       },
